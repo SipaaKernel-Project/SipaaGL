bddea577e020880abda873a41140fd78bb9a4ba7
Raphael <raphaelmartinaud@hotmail.com>
2023-02-22 17:51:17 +0100

    Test
    GitHub actions test

---
+++ SipaaGL/Blur.cs
@@ -0,0 +1,99 @@
+﻿using Cosmos.Core;
+using Cosmos.Core.Memory;
+using Cosmos.System.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SipaaGL
+{
+    /// <summary>
+    /// RaphMar2019's blur implementation
+    /// 
+    /// Made by nifanfa, Ported to SipaaGL by me
+    /// </summary>
+    public unsafe class Blur
+    {
+        public static void BlurGraphics(Graphics gr, uint intensity)
+        {
+            uint* _raw = gr.Internal;
+
+            for (uint w = 0; w < gr.Width; w++)
+            {
+                for (uint h = 0; h < gr.Height; h++)
+                {
+                    long r = 0, g = 0, b = 0, a = 0;
+                    int counter = 0;
+
+                    for (uint ww = w - intensity; ww < w + intensity; ww++)
+                    {
+                        for (uint hh = h - intensity; hh < h + intensity; hh++)
+                        {
+                            if (ww >= 0 && hh >= 0 && ww < gr.Width && hh < gr.Height)
+                            {
+                                Color color = Color.FromARGB(_raw[gr.Width * hh + ww]);
+
+                                r += color.R;
+                                g += color.G;
+                                b += color.B;
+                                a += color.A;
+
+                                counter++;
+                            }
+                        }
+                    }
+
+                    r /= counter;
+                    g /= counter;
+                    b /= counter;
+                    a /= counter;
+
+                    gr[(int)w, (int)h] = Color.FromARGB((byte)a, (byte)r, (byte)g, (byte)b).ARGB;
+                }
+            }
+        }
+
+        public static void BlurGraphicsRegion(Graphics gr, int x, int y, int width, int height, uint intensity)
+        {
+            uint* _raw = gr.Internal;
+
+            for (uint w = (uint)x; w < width; w++)
+            {
+                for (uint h = (uint)y; h < height; h++)
+                {
+                    long r = 0, g = 0, b = 0, a = 0;
+                    int counter = 0;
+
+                    for (uint ww = w - intensity; ww < w + intensity; ww++)
+                    {
+                        for (uint hh = h - intensity; hh < h + intensity; hh++)
+                        {
+                            if (ww >= 0 && hh >= 0 && ww < gr.Width && hh < gr.Height)
+                            {
+                                Color color = Color.FromARGB(_raw[gr.Width * hh + ww]);
+
+                                r += color.R;
+                                g += color.G;
+                                b += color.B;
+                                a += color.A;
+
+                                counter++;
+                            }
+                        }
+                    }
+
+                    r /= counter;
+                    g /= counter;
+                    b /= counter;
+                    a /= counter;
+
+                    gr[(int)w, (int)h] = Color.FromARGB((byte)a, (byte)r, (byte)g, (byte)b).ARGB;
+                }
+            }
+        }
+    }
+}
--- ostest/Kernel.cs
+++ ostest/Kernel.cs
@@ -16,6 +16,7 @@ namespace ostest
         public static byte[] sklogo;
 
         Graphics img;
+        Gradient g;
 
         SVGAIICanvas C;
         AnimationController A = new(25f, 270f, new(0, 0, 0, 0, 750), AnimationMode.Ease);
@@ -33,8 +34,11 @@ namespace ostest
 
             X = C.Width / 2;
             Y = C.Height / 2 + 100;
+            g = new(100, 100, new Color[] { Color.Blue, Color.White });
 
             img = Image.FromBitmap(sklogo);
+            //GaussianBlur.BlurGraphics(img, 20);
+            Blur.BlurGraphicsRegion(img, 0, 0, 64, 64, 4);
         }
 
         protected override void Run()
@@ -56,6 +60,7 @@ namespace ostest
             C.DrawImage(X - 128 / 2, Y - 200, img, true);
             C.DrawArc(X, Y, 20, Color.White, Offset, LengthOffset);
             C.DrawString(10, 10, $"{C.GetFPS()} FPS", BitFont.Fallback, Color.White);
+            C.DrawImage(100, 100, g, true);
             C.DrawFilledRectangle((int)MouseManager.X, (int)MouseManager.Y, 10, 10, 0, Color.White);
             C.Update();
         }
